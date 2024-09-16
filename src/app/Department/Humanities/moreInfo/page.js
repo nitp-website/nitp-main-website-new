9b7728a6d808bac70e1c5562f9664f454d3aac29
Devraj Kumar <devrajkumar@Devrajs-MacBook-Air.local>
2024-09-16 18:06:01 +0530

    more

--- src/app/Department/Humanities/moreInfo/page.js
+++ src/app/Department/Humanities/moreInfo/page.js
@@ -22,106 +22,123 @@ function Page(){
             <div className=" border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md">
                 <div className="mb-10 text-black overflow-x-auto">
                     <div className="w-[1300px]">
-                        <div className="font-bold mb-2 text-red-900">List of Short - Term Courses / Faculty Development Program / Workshop Seminar/Conferences/Guest Lectures organised:</div>
+                        <div className="font-bold mb-2 text-red-900">Research Scholars Information</div>
                         <div className=" grid grid-cols-12  border border-black">
                             <div className="px-2 h-10 flex flex-col justify-center col-span-1 border-r border-black font-bold">S.no.</div>
-                            <div className="px-2 h-10 flex flex-col justify-center col-span-4 border-r border-black font-bold">Name of the Faculty</div>
-                            <div className="px-2 h-10 flex flex-col justify-center col-span-5 border-r border-black font-bold">Programme Name</div>
-                            <div className="px-2 h-10 flex flex-col justify-center col-span-2  font-bold">Duration</div>
+                            <div className="px-2 h-10 flex flex-col justify-center col-span-1 border-r border-black font-bold">Roll</div>
+                            <div className="px-2 h-10 flex flex-col justify-center col-span-4 border-r border-black font-bold">	Name of the Scholar</div>
+                            <div className="px-2 h-10 flex flex-col justify-center col-span-4 border-r border-black font-bold">	Name of the Supervisor</div>
+                            <div className="px-2 h-10 flex flex-col justify-center col-span-2  font-bold">Status</div>
                         </div>
                         <div className="font-bold font-serif my-2">Full-Time (Institute Fellowship)</div>
                         <div className=" grid grid-cols-12  border border-black  text-[12px]">
                             <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black">1</div>
+                            <div className="px-2 flex flex-col justify-center col-span-1 border-r border-black"></div>
                             <div className="px-2 flex flex-col justify-center col-span-4 border-r border-black"></div>
-                            <div className="px-2  flex flex-col justify-center col-span-5 border-r border-black"></div>
+                            <div className="px-2  flex flex-col justify-center col-span-4 border-r border-black"></div>
                             <div className="px-2  flex flex-col justify-center col-span-2 "></div>
                         </div>
                         <InfoCompo 
                             sn={"2"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"3"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"4"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"5"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"6"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"7"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"8"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"9"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"10"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"11"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"12"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"13"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"14"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"15"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"16"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
@@ -130,30 +147,35 @@ function Page(){
                         <div className="font-bold font-serif my-2">Full-Time (Sponsored)</div>
                         <div className=" grid grid-cols-12  border border-black  text-[12px]">
                             <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black">1</div>
+                            <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black"></div>
                             <div className="px-2 flex flex-col justify-center col-span-4 border-r border-black"></div>
-                            <div className="px-2  flex flex-col justify-center col-span-5 border-r border-black"></div>
+                            <div className="px-2  flex flex-col justify-center col-span-4 border-r border-black"></div>
                             <div className="px-2  flex flex-col justify-center col-span-2 "></div>
                         </div>
                         <InfoCompo 
                             sn={"2"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"3"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"4"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
                         />
                         <InfoCompo 
                             sn={"5"}
+                            roll={""}
                             scholar={""}
                             suparvisor={""}
                             status={""}
@@ -165,12 +187,13 @@ function Page(){
     )
 }
 
-function InfoCompo({sn, scholar, suparvisor, status}){
+function InfoCompo({sn, roll, scholar, suparvisor, status}){
     return (
         <div className=" grid grid-cols-12  border border-black border-t-0 text-[12px]">
             <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black">{sn}</div>
+            <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black">{roll}</div>
             <div className="px-2 flex flex-col justify-center col-span-4 border-r border-black">{scholar}</div>
-            <div className="px-2  flex flex-col justify-center col-span-5 border-r border-black">{suparvisor}</div>
+            <div className="px-2  flex flex-col justify-center col-span-4 border-r border-black">{suparvisor}</div>
             <div className="px-2  flex flex-col justify-center col-span-2 ">{status}</div>
         </div>
     )
