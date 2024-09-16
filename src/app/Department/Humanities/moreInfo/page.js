304912d89f35cdd03fb5d5e64a02c3cab71f4300
Nishant kumar <nishantkumarfps@gmail.com>
2024-09-16 18:42:16 +0530

    Completed the more info page of humanities department

--- src/app/Department/Humanities/moreInfo/page.js
+++ src/app/Department/Humanities/moreInfo/page.js
@@ -33,152 +33,152 @@ function Page(){
                         <div className="font-bold font-serif my-2">Full-Time (Institute Fellowship)</div>
                         <div className=" grid grid-cols-12  border border-black  text-[12px]">
                             <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black">1</div>
-                            <div className="px-2 flex flex-col justify-center col-span-1 border-r border-black"></div>
-                            <div className="px-2 flex flex-col justify-center col-span-4 border-r border-black"></div>
-                            <div className="px-2  flex flex-col justify-center col-span-4 border-r border-black"></div>
-                            <div className="px-2  flex flex-col justify-center col-span-2 "></div>
+                            <div className="px-2 flex flex-col justify-center col-span-1 border-r border-black">185HS01</div>
+                            <div className="px-2 flex flex-col justify-center col-span-4 border-r border-black">SHALU KUMARI</div>
+                            <div className="px-2  flex flex-col justify-center col-span-4 border-r border-black">Dr. Manish Tiwari</div>
+                            <div className="px-2  flex flex-col justify-center col-span-2 ">Submitted</div>
                         </div>
                         <InfoCompo 
                             sn={"2"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"195HS01"}
+                            scholar={"TALAT"}
+                            suparvisor={"Dr. Sukhdev Singh"}
+                            status={"Submitted"}
                         />
                         <InfoCompo 
                             sn={"3"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"195HS08"}
+                            scholar={"SANA ASIF"}
+                            suparvisor={"Dr. Sukhdev Singh"}
+                            status={"Submitted"}
                         />
                         <InfoCompo 
                             sn={"4"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"205HS02"}
+                            scholar={"MUGDHA MOHINI"}
+                            suparvisor={"Dr. Ashish Ranjan Sinha"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"5"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"205HS011"}
+                            scholar={"DIPAK KUMAR MANDAL"}
+                            suparvisor={"Dr. Zeeshan Ali"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"6"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"205HS012"}
+                            scholar={"RAMA SHANKAR SAHU"}
+                            suparvisor={"Dr. Manish Tiwari"}
+                            status={"Submitted"}
                         />
                         <InfoCompo 
                             sn={"7"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"205HS013"}
+                            scholar={"DHRUVEE SINHA"}
+                            suparvisor={"Dr. Zeeshan Ali"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"8"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"205HS014"}
+                            scholar={"SHAIL KUMARI"}
+                            suparvisor={"Dr. Zeeshan Ali"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"9"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"215HS001"}
+                            scholar={"ASHUTOSH YADAV"}
+                            suparvisor={"Dr. Deepak Kumar Behera"}
+                            status={"Submitted"}
                         />
                         <InfoCompo 
                             sn={"10"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"215HS002"}
+                            scholar={"SNEHA DAS"}
+                            suparvisor={"Dr. Zeeshan Ali"}
+                            status={"Pre-submission conducted"}
                         />
                         <InfoCompo 
                             sn={"11"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"215HS003"}
+                            scholar={"PRITAM RANJAN SAHU"}
+                            suparvisor={"Dr. Deepak Kumar Behera"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"12"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"215HS009"}
+                            scholar={"VIJAY KUMAR"}
+                            suparvisor={"Dr. Ashish Ranjan Sinha"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"13"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"225HS008"}
+                            scholar={"JAI DEV"}
+                            suparvisor={"Dr. Zeeshan Ali (Supervisor) & Dr. Ashish Ranjan Sinha (Co-Supervisor)"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"14"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"235HS001"}
+                            scholar={"SAURABH ANAND"}
+                            suparvisor={"Dr. Manish Tiwari"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"15"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"235HS003"}
+                            scholar={"SAKSHI KUMARI"}
+                            suparvisor={"Dr. Ashish Ranjan Sinha"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"16"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"235HS004"}
+                            scholar={"RAVEESH RAJNI"}
+                            suparvisor={"Dr. Deepak Kumar Behera"}
+                            status={"Ongoing"}
                         />
 
                         <div className="font-bold font-serif my-2">Full-Time (Sponsored)</div>
                         <div className=" grid grid-cols-12  border border-black  text-[12px]">
                             <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black">1</div>
-                            <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black"></div>
-                            <div className="px-2 flex flex-col justify-center col-span-4 border-r border-black"></div>
-                            <div className="px-2  flex flex-col justify-center col-span-4 border-r border-black"></div>
-                            <div className="px-2  flex flex-col justify-center col-span-2 "></div>
+                            <div className="px-2  flex flex-col justify-center col-span-1 border-r border-black">225HS001</div>
+                            <div className="px-2 flex flex-col justify-center col-span-4 border-r border-black">AMIT KUMAR </div>
+                            <div className="px-2  flex flex-col justify-center col-span-4 border-r border-black">Dr. Ashish Ranjan Sinha</div>
+                            <div className="px-2  flex flex-col justify-center col-span-2 ">Ongoing</div>
                         </div>
                         <InfoCompo 
                             sn={"2"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"225HS002"}
+                            scholar={"AUGUST KESHAV"}
+                            suparvisor={"Dr. Ashish Ranjan Sinha"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"3"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"225HS007"}
+                            scholar={"ANSHU KUMARI"}
+                            suparvisor={"Dr. Manish Tiwari"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"4"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"225HS013"}
+                            scholar={"DEEPIKA KANTH "}
+                            suparvisor={"Dr. Ashish Ranjan Sinha"}
+                            status={"Ongoing"}
                         />
                         <InfoCompo 
                             sn={"5"}
-                            roll={""}
-                            scholar={""}
-                            suparvisor={""}
-                            status={""}
+                            roll={"235HS002"}
+                            scholar={"MD. SHABBIR ALAM"}
+                            suparvisor={"Dr. Deepak Kumar Behera"}
+                            status={"Ongoing"}
                         />
                     </div>
                 </div>
