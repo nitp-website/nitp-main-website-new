4a7d19db68fa10a3feecb87d535050e6cf015d35
Ashish <kumarashish98526@gmail.com>
2024-09-19 20:25:56 +0530

    "Refactored Humanities page to fetch notices from API, updated DepartmentNotify component to parse link, and created new DepartmentNotify1 component with attachment rendering logic"

--- src/app/components/department/DepartmentNotify.js
+++ src/app/components/department/DepartmentNotify.js
@@ -1,26 +1,37 @@
-
+import React from 'react';
 
 function DepartmentNotify(props) {
-    const link = props.link;
-    if(link && link.length > 5){
+    let link = '';
+
+    // Parse the link if it's a JSON string
+    if (props.link) {
+        try {
+            const parsedLink = JSON.parse(props.link);
+            link = parsedLink.url || '';
+        } catch (error) {
+            console.error('Error parsing link:', error);
+        }
+    }
+
+    if (link && link.length > 5) {
         return (
-            <a href={`${link}`} className=" my-1 font-semibold text-[14px] flex text-start hover:text-blue-500"> 
+            <a href={link} className="my-1 font-semibold text-[14px] flex text-start hover:text-blue-500">
                 <div className="flex items-center">
-                    <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div> 
+                    <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div>
                     {props.title}
                 </div>
             </a>
-        )
+        );
     }
+
     return (
         <div className="my-1 flex text-start">
             <div className="flex items-center">
-                <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div> 
+                <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div>
                 {props.title}
             </div>
         </div>
-    )
-
+    );
 }
 
-export default DepartmentNotify
+export default DepartmentNotify;
--- src/app/components/department/DepartmentNotify1.js
+++ src/app/components/department/DepartmentNotify1.js
@@ -1,67 +1,54 @@
 import { useEffect, useState } from "react";
 
-
 function DepartmentNotify1(props) {
-    const link = props.link;
+    const link = props.link ? JSON.parse(props.link).url : '';
     const color = ["yellow-700", "black"];
     const [textCol, settextCol] = useState("black");
-    var flag = 0;
-    useEffect(()=>{
-        const interval = setInterval(()=>{
-            flag -= 1;
-            if(flag < 0){
-                flag = 1;
-            }
+    let flag = 0;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            flag = flag === 0 ? 1 : 0;
             settextCol(color[flag]);
-        },1000)
-        return ()=> clearInterval(interval)
-    },[])
-    if(link && link.length > 5){
+        }, 1000);
+        
+        return () => clearInterval(interval);
+    }, []);
+
+    if (link && link.length > 5) {
         return (
             <div>
-                <a href={`${link}`} className={`${(props.important === 1)? `text-${textCol}`:""} my-1 font-semibold text-[14px] flex text-start hover:text-blue-500`}> 
+                <a href={link} className={`${(props.important === 1) ? `text-${textCol}` : ""} my-1 font-semibold text-[14px] flex text-start hover:text-blue-500`}>
                     <div className="flex items-center">
-                        <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div> 
+                        <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div>
                         {props.title}
                     </div>
                 </a>
-                {/* {!(props.attachments.length > 0)?
-                <div className="ml-10">
-                    {attachments.map((att)=>{
-                        return (
-                            <AttachmentsCompo 
-                                {title = att.caption || ""}
-                                {link = at.url}
-                            />
-                        )
-                    })}
-                </div>
-                :
-                <div></div>} */}
+                {/* Attachments rendering logic can go here */}
             </div>
-        )
+        );
     }
+    
     return (
         <div className="my-1 flex text-start">
             <div className="flex items-center">
-                <div className={`${(props.important === 1)? `text-${textCol}`:""} border rounded-full bg-black h-[10px] w-[10px] mr-2`}></div> 
+                <div className={`${(props.important === 1) ? `text-${textCol}` : ""} border rounded-full bg-black h-[10px] w-[10px] mr-2`}></div>
                 {props.title}
             </div>
         </div>
-    )
-
+    );
 }
 
-function AttachmentsCompo(props){
+function AttachmentsCompo(props) {
     const link = props.link;
-    return(
-        <a href={`${link}`} className={`${(props.important === 1)? `text-${textCol}`:""} my-1 font-semibold text-[14px] flex text-start hover:text-blue-500`}> 
+    return (
+        <a href={link} className={`${(props.important === 1) ? `text-${textCol}` : ""} my-1 font-semibold text-[14px] flex text-start hover:text-blue-500`} target="_blank">
             <div className="flex items-center">
-                <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div> 
+                <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div>
                 {props.title}
             </div>
         </a>
-    )
+    );
 }
 
-export default DepartmentNotify1
+export default DepartmentNotify1;
