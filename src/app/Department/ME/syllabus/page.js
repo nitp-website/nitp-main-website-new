1861d86ac34d6593cce61c0421012f0eea6c0101
National Insitute of Technology Patna <162422736+nitp-website@users.noreply.github.com>
2024-09-24 23:02:31 +0530

    Update page.js

--- src/app/Department/ME/syllabus/page.js
+++ src/app/Department/ME/syllabus/page.js
@@ -7,20 +7,20 @@ export default function page() {
             <div className="flex flex-col  mb-6">
                 <div>
                     <p className="text-red-900 text-xl lg:text-2xl font-bold">
-                        SYLLABUS
+                        Syllabus 
                     </p>
                 </div>
                 <div className="lg:mt-2">
                     <p className="text-gray-500 text-sm lg:text-base font-semibold">
-                        MECHANICAL ENGINEERING
+                        Mechanical Engineering 
                     </p>
                 </div>
                 <BackDepartment navigate={"/Department/ME"} />
             </div>
             <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md">
-                {/* UG PROGRAM */}
+                {/* UG Program */}
                 <div className="mb-10">
-                    <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">UG PROGRAM :-</div>
+                    <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">UG Program  :-</div>
                     <ul className="list-disc list-inside ml-5 space-y-4 lg:text-base text-sm">
                         <li className="hover:text-blue-800"><a href="">B.Tech_Course_Structure_2021 - (2024 April update)</a></li>
                         <li className="hover:text-blue-800"><a href="">B.Tech_Course_Structure_2022 onwards - (2024 April update)</a></li>
@@ -29,7 +29,7 @@ export default function page() {
                         <li className="hover:text-blue-800"><a href="">B.Tech 1st Year Odd Semester Syllabus</a></li>
                     </ul>
                 </div>
-                {/* Dual degree PROGRAM  */}
+                {/* Dual Degree Program  */}
                 <div className="mb-10">
                     <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">DUAl DEGREE PROGRAM :-</div>
                     <ul className="list-disc list-inside ml-5 space-y-4 lg:text-base text-sm">
@@ -37,7 +37,7 @@ export default function page() {
                         <li className="hover:text-blue-800"><a href="">Dual Degree ( B.Tech + M.Tech) _ Detailed Syllabus _ 3rd Semester Onwards.</a></li>
                     </ul>
                 </div>
-                {/* pg PROGRAM  */}
+                {/* PG Program   */}
                 <div className="mb-10">
                     <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">PG PROGRAM :-</div>
                     <ul className="list-disc list-inside ml-5 space-y-4 lg:text-base text-sm">
@@ -45,7 +45,7 @@ export default function page() {
                         <li className="hover:text-blue-800"><a href="">M.Tech _ Detailed Syllabus _ All Specializations.</a></li>
                     </ul>
                 </div>
-                {/* PHD PROGRAM  */}
+                {/* PhD Program   */}
                 <div className="mb-10">
                     <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">PHD PROGRAM :-</div>
                     <ul className="list-disc list-inside ml-5 space-y-4 lg:text-base text-sm">
