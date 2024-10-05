ddea09fba18a50c6b5ec249dfc8f89025eeafce9
Siddhant <sidisan0711@gmail.com>
2024-10-05 18:51:19 +0530

    slide images in sports section added

--- src/app/Facilities/SportsFacilities/page.js
+++ src/app/Facilities/SportsFacilities/page.js
@@ -6,8 +6,8 @@ import Carousel from '../../../components/sportsComponents/Carousel'
 
 export default function Home() {
   var labUrl = [
-    "https://i.postimg.cc/Pfbj3bYD/DSC09197.jpg",
-    "https://i.postimg.cc/xjB6zBcy/4.jpg",
+    "https://i.postimg.cc/j2Rcm5BV/4.jpg",
+    "https://i.postimg.cc/zXXpkynK/DSC09197.jpg",
   ]
   return (
     <div className='flex flex-row p-10 mx-4'>
@@ -17,14 +17,15 @@ export default function Home() {
             NIT PATNA SPORTS
           </p>
         </div>
-        <div className='w-[1000px] border-2 border-black'>
-          <Carousel autoSlide={true}>
+        <div className='lg:w-[1000px] lg:h-[400px] w-[300px] border-2 border-black'>
+          {/* <Carousel autoSlide={true}>
             {labUrl.map((img,i)=>(     
               <Image src={img} alt='image' width={1000}
-              height={500} key={i} className='object-fill'
+              height={500} key={i} className='object-cover'
               />     
             ))}
-          </Carousel>
+          </Carousel> */}
+          <Carousel/>
         </div>
         <div>
           
--- src/components/sportsComponents/Carousel.js
+++ src/components/sportsComponents/Carousel.js
@@ -1,30 +1,75 @@
+// "use client"
+// import React,{useState,useEffect} from 'react'
+
+// function Carousel({children:pics,
+//     autoSlideInterval=3000,
+//     autoSlide=true,}){
+
+//     const [curr,setCurr]=useState(0)
+//     const prev=()=>{
+//         setCurr((curr)=>(curr===0?pics.length-1:curr-1))
+//     }
+//     const next=()=>{
+//         setCurr((curr)=>(curr===pics.length-1?0:curr+1))
+//     }
+//     useEffect(()=>{
+//         if(!autoSlide) return 
+//         const slideInterval=setInterval(next,autoSlideInterval)
+//         return ()=>clearInterval(slideInterval)
+//     },[])
+//     return(
+//         <div className=' relative overflow-hidden'>
+//             <div className='flex transition-transform ease-out duration-1000'
+//             style={{transform:`translateX(-${curr*100}%)`}}>
+//                 {pics}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Carousel
 "use client"
-import React,{useState,useEffect} from 'react'
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-function Carousel({children:pics,
-    autoSlideInterval=3000,
-    autoSlide=true,}){
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-    const [curr,setCurr]=useState(0)
-    const prev=()=>{
-        setCurr((curr)=>(curr===0?pics.length-1:curr-1))
-    }
-    const next=()=>{
-        setCurr((curr)=>(curr===pics.length-1?0:curr+1))
-    }
-    useEffect(()=>{
-        if(!autoSlide) return 
-        const slideInterval=setInterval(next,autoSlideInterval)
-        return ()=>clearInterval(slideInterval)
-    },[])
-    return(
-        <div className=' relative overflow-hidden'>
-            <div className='flex transition-transform ease-out duration-1000'
-            style={{transform:`translateX(-${curr*100}%)`}}>
-                {pics}
-            </div>
-        </div>
-    )
+import '../../components/sportsComponents/carousel.css'
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+const Carousel = () => {
+    return (
+        <>
+        
+          <Swiper
+            spaceBetween={0}
+            centeredSlides={true}
+            autoplay={{
+              delay: 3500,
+              disableOnInteraction: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="mySwiper"
+          >
+             <SwiperSlide><img src="https://i.postimg.cc/j2Rcm5BV/4.jpg" /></SwiperSlide>      
+            <SwiperSlide><img src="https://i.postimg.cc/Pfbj3bYD/DSC09197.jpg" /></SwiperSlide>
+            <SwiperSlide><img src="https://i.postimg.cc/0jDVqSGQ/Image-1-00001.jpg" /></SwiperSlide>
+
+            
+            
+          </Swiper>
+        </>
+      );
 }
 
 export default Carousel
