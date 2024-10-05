8a675cfbe988c60af53a12ef219d060fbc3eb1e6
Anurag Bhanu <anurag92017@gmail.com>
2024-10-05 17:40:49 +0530

    Sidebar

--- src/app/Facilities/SportsFacilities/page.js
+++ src/app/Facilities/SportsFacilities/page.js
@@ -3,35 +3,32 @@ import SideBar from '../../../components/sportsComponents/SideBar'
 import Image from 'next/image';
 import Carousel from '../../../components/sportsComponents/Carousel'
 
-
 export default function Home() {
   var labUrl = [
     "https://i.postimg.cc/Pfbj3bYD/DSC09197.jpg",
     "https://i.postimg.cc/xjB6zBcy/4.jpg",
   ]
+  
   return (
-    <div className='flex flex-row p-10 mx-4'>
+    <div className='flex flex-col md:flex-row p-8 mx-4'>
       <div className='flex flex-col'>
         <div>
           <p className='text-2xl font-semibold'>
             NIT PATNA SPORTS
           </p>
         </div>
-        <div className='w-[1000px] border-2 border-black'>
+        <div className='w-full md:w-[900px] border-2 border-black'>
           <Carousel autoSlide={true}>
             {labUrl.map((img,i)=>(     
-              <Image src={img} alt='image' width={1000}
-              height={500} key={i} className='object-fill'
-              />     
+              <Image src={img} alt='image' width={900} height={500} key={i} className='object-fill' />     
             ))}
           </Carousel>
         </div>
-        <div>
-          
-        </div>
       </div>
-      <div className='w-1/3'>
-        <SideBar/>
+
+      {/* Sidebar */}
+      <div className='mt-6 md:mt-0 md:ml-6'>
+        <SideBar />
       </div>
     </div>
   );
---
+++ src/components/sportsComponents/PeopleList.js
@@ -0,0 +1,95 @@
+"use-client";
+import PeopleCard from './PeopleCard'
+
+export default function PeopleList() {
+    const Deanery = [
+      {
+        id: 1,
+        name: "Dr. John Doe",
+        image: "",
+        designation: "Professor of Computer Science",
+        email: "johndoe@example.com",
+        ext_no: "1234",
+      },
+      {
+        id: 2,
+        name: "Dr. Jane Smith",
+        image: "",
+        designation: "Assistant Professor of Mathematics",
+        email: "janesmith@example.com",
+        ext_no: "5678",
+      },
+    ];
+
+    const administration = [
+      {
+        id: 1,
+        name: "Dr. John Doe",
+        image: "",
+        designation: "Professor of Computer Science",
+        email: "johndoe@example.com",
+        ext_no: "1234",
+      },
+      {
+        id: 2,
+        name: "Dr. Jane Smith",
+        image: "",
+        designation: "Assistant Professor of Mathematics",
+        email: "janesmith@example.com",
+        ext_no: "5678",
+      },
+      {
+        id: 3,
+        name: "Dr. Emily Johnson",
+        image: "",
+        designation: "Lecturer of Physics",
+        email: "emilyjohnson@example.com",
+        ext_no: "9101",
+      },
+      {
+        id: 4,
+        name: "Dr. Michael Brown",
+        image: "",
+        designation: "Senior Lecturer of Chemistry",
+        email: "michaelbrown@example.com",
+        ext_no: "1123",
+      }
+    ]
+  
+    return (
+      <div className="flex flex-col p-4 m-4">
+      <div key="People">
+        <h6 className="font-bold text-black">Deanery</h6>
+        <div className="grid md:grid-cols-2 gap-1">
+          {Deanery.map((faculty) => (
+            <PeopleCard
+              key={faculty.id}
+              name={faculty.name}
+              image={faculty.image}
+              designation={faculty.designation}
+              email={faculty.email}
+              phone={faculty.ext_no}
+            />
+          ))}
+        </div>
+        <div key="Administration">
+        <h6 className="font-bold text-black">Administration</h6>
+        <div className="grid md:grid-cols-2 gap-1">
+          {administration.map((faculty) => (
+            <PeopleCard
+              key={faculty.id}
+              name={faculty.name}
+              image={faculty.image}
+              designation={faculty.designation}
+              email={faculty.email}
+              phone={faculty.ext_no}
+            />
+          ))
+          }
+        </div>
+      </div>
+      </div>
+      </div>
+    );
+  }
+  
