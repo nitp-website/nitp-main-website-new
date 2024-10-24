75ccc004c76d249664e2f2e19c8797cab15798af
Ashish <kumarashish98526@gmail.com>
2024-10-24 10:02:20 +0530

    "Added Intrannet and Added time table data and table component to ECE department page"

---
+++ src/app/Academic/Intranet/page.js
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import "./intranetRedirect.css";
+
+const IntranetRedirect = () => {
+  const [seconds, setSeconds] = useState(6); // Countdown starts from 6 seconds
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+
+    const redirect = setTimeout(() => {
+      window.location.href = "http://192.168.1.16:3333"; // Redirect to your intranet site
+    }, 6000); // 6 seconds redirect
+
+    return () => {
+      clearInterval(timer);
+      clearTimeout(redirect);
+    };
+  }, []);
+
+  return (
+    <div id="intranet-redirect">
+      <div className="intranet text-neutral-700">
+        <div className="intranet-info">
+          <h1>Welcome to NIT Intranet</h1>
+        </div>
+        <h2 className="pt-10 text-red-700">
+          Connect to NIT Network (WiFi or Ethernet)
+        </h2>
+        <p>
+          Please connect to the NIT Network to access the intranet website. You
+          will be redirected in {seconds} seconds.
+        </p>
+        <p>If the redirection does not work, click below:</p>
+        <a href="http://192.168.1.16:3333" className="intranet-link">
+          Go To NIT Intranet
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default IntranetRedirect;
--- src/app/Department/ECE/timeTable/page.js
+++ src/app/Department/ECE/timeTable/page.js
@@ -2,6 +2,30 @@ import BackDepartment from "../../../components/department/BackDepartment"
 
 
 function Page(){
+    const data = [
+        { 
+          name: "1st Semester ECE Time Table", 
+          link: "https://drive.google.com/file/d/1tZpHlXHhBhDIOTRm--cPYKFqRlJ_95zz/view?usp=drivesdk" 
+        },
+        
+        { 
+          name: "3rd Sem ECE Sec A_B Time Table", 
+          link: "https://drive.google.com/file/d/1gCp3bS01PsWZNAs8H5oudFQbj1Ucvu8V/view?usp=drivesdk" 
+        },
+        { 
+            name: "3rd Semester Sec A1 ECE Time Table", 
+            link: "https://drive.google.com/file/d/1qxhZTfbDmX1_5-XuBEd9yCqWgqQ4Vtwx/view?usp=drivesdk" 
+          },
+        { 
+          name: "5th Semester ECE Time Table", 
+          link: "https://drive.google.com/file/d/1eaKpE_iQFXeUNXW9GnvoGaZO56EAvWr0/view?usp=drivesdk" 
+        },
+        
+        { 
+            name: "7th Semester ECE Time Table", 
+            link: "https://drive.google.com/file/d/1luNXXj1_jS5XE8lypF9n_SVCXJKJtfhy/view?usp=drivesdk" 
+          },
+      ];
     return (
         <div className=" flex flex-col p-5 h-screen text-black">  
             <div className="flex flex-col md:ml-10">
@@ -19,13 +43,29 @@ function Page(){
                 <BackDepartment navigate={'/Department/ECE'}/>
                 </div>
             </div>
-            <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md h-screen">
-                <div className='mt-14 '>
-                    {/* <a href={'https://www.dropbox.com/scl/fo/kmdu6u1erwn18lwo0qe4p/h?rlkey=rl1xo6nzihg0j5yc9zvfidfr0&dl=0'} className='lg:text-lg text-gray-500 font-semibold border-b-2 border-black hover:text-blue-500' target="_blank">
-                        Click here for the time table of CSED (for all semesters)
-                    </a> */}
-                </div>
-            </div>
+            <div className="md:mx-auto px-4 py-8 w-4/5">
+      
+      <table className="w-full mx-auto border-collapse border border-neutral-600 bg-white rounded-lg">
+        <thead>
+          <tr>
+            <th className="text-left px-2 py-2 md:px-4 md:py-4 bg-gradient-to-r from-red-200 to-pink-200 text-black">Name</th>
+            <th className="text-center px-2 py-2 md:px-4 md:py-4 bg-gradient-to-r from-red-200 to-orange-200 text-black">Download Now</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td className="text-xs md:text-base text-left px-2 md:px-4 py-2 border-b border-gray-300 text-black">{item.name}</td>
+              <td className="text-xs md:text-base text-center px-4 py-3 border-b border-gray-300">
+                <a href={item.link} className="bg-blue-400 text-white font-bold px-4 py-2 rounded-md hover:bg-gradient-to-r from-cyan-500 to-blue-500 md:text-xs text-xs" target="_blank">
+                  Download
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
         </div>
 
     )
