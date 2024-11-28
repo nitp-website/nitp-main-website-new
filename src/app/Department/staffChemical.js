e011036d3738f0819a52a1b0be273df69dbef5e1
Pankaj <pankaj8920858327@gamil.com>
2024-11-28 23:12:21 +0530

    update staff cse and chemical

---
+++ src/app/Department/staffChemical.js
@@ -0,0 +1,20 @@
+const staffData = [
+    {
+        department: "Chemical",
+        staff: [
+            {
+                name: "Mr. Debabrata De",
+                designation: "Technician",
+                experience: "Worked as NMR operator at IIT Patna for 1.6 years",
+                education: "MSc",
+                expertise: "Organic Chemistry",
+                email: "debabrata.tech@nitp.ac.in",
+                mobile: "+91-",
+                image: "/staffChemical.jpg",
+            },
+            
+        ],
+    },
+];
+
+export default staffData;
