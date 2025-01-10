209a23ef70257388523c891274fb25842d94c0d6
Pankaj <pankaj8920858327@gamil.com>
2025-01-10 10:22:51 +0530

    Update ME Syllabus

--- src/app/Department/Humanities/time-table/page.js
+++ src/app/Department/Humanities/time-table/page.js
@@ -1,33 +1,62 @@
+
 import BackDepartment from "../../../components/department/BackDepartment"
 
 
-function page() {
+function Page() {
+    const data = [
+        {
+            name: "Humanities & Social Sciences Dept Time Table for the Class July- December 2024 (odd Semster)",
+            link: "https://drive.google.com/file/d/1c6f2tp-MMqf9SzPudHyerB7NfdDB4ffo/view"
+        },
+        {
+            name: "Humanities & Social Sciences Dept Time Table for the Class Jan- June 2025 (Even Semster)",
+            link: "https://drive.google.com/file/d/106cCdw6SbsN1GIjM4reh3WY8XzsmkCHO/view?usp=sharing"
+        },
+
+
+    ];
     return (
-        <div className=" flex flex-col p-10 max-sm:p-2 h-screen text-black">
-            <div className="flex flex-col  mb-6">
+        <div className=" flex flex-col p-5 h-screen text-black">
+            <div className="flex flex-col md:ml-10">
                 <div>
-                    <p className="text-red-900 text-xl lg:text-2xl font-bold">
-                        Time Table
+                    <p className="text-red-900 text-3xl font-bold">
+                        TIME TABLE
                     </p>
                 </div>
-                <div className=" lg:mt-2">
-                    <p className="text-gray-500 text-sm lg:text-base font-semibold">
+                <div className="mt-2">
+                    <p className="text-gray-500 text-xl font-semibold">
                         Humanities & Social Sciences
                     </p>
                 </div>
-                <BackDepartment navigate={"/Department/Humanities"} />
-            </div>
-            <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md h-screen">
-                {/* LINKS */}
-                <div className="mb-10">
-                    <ul className="list-disc list-inside ml-5 space-y-4 text-lg max-md:text-[15px] max-sm:text-sm">
-                        <li className="hover:text-blue-800"><a href="https://drive.google.com/file/d/1c6f2tp-MMqf9SzPudHyerB7NfdDB4ffo/view">
-                        Click here for the Master_Time-Table_ HSS_July-December 2024.</a></li>
-                        
-                    </ul>
+                <div>
+                    <BackDepartment navigate={'/Department/Humanities'} />
                 </div>
             </div>
+            <div className="md:mx-auto px-4 py-8 w-4/5">
+
+                <table className="w-full mx-auto border-collapse border border-neutral-600 bg-white rounded-lg">
+                    <thead>
+                        <tr>
+                            <th className="text-left px-2 py-2 md:px-4 md:py-4 bg-gradient-to-r from-red-200 to-pink-200 text-black">Name</th>
+                            <th className="text-center px-2 py-2 md:px-4 md:py-4 bg-gradient-to-r from-red-200 to-orange-200 text-black">Download Now</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => (
+                            <tr key={index}>
+                                <td className="text-xs md:text-base text-left px-2 md:px-4 py-2 border-b border-gray-300 text-black">{item.name}</td>
+                                <td className="text-xs md:text-base text-center px-4 py-3 border-b border-gray-300">
+                                    <a href={item.link} className="bg-blue-400 text-white font-bold px-4 py-2 rounded-md hover:bg-gradient-to-r from-cyan-500 to-blue-500 md:text-xs text-xs" target="_blank">
+                                        Download
+                                    </a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
+
     )
 }
-export default page
+export default Page
