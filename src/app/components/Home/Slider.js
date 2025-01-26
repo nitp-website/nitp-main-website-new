55caa47ec274c4de728292e81a5d956910e52794
AakashTiwari-nitp <aakasht.ug23.cs@nitp.ac.in>
2025-01-26 09:09:40 +0530

    flag image added

--- src/app/components/Home/Slider.js
+++ src/app/components/Home/Slider.js
@@ -1,52 +1,72 @@
-"use client"
-import React, { useRef, useState } from 'react';
+"use client";
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import flagimg from "../../../../public/flag.jpeg"
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-import './styles/Slider.css';
+import "./styles/Slider.css";
 
 // import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Slider = () => {
-    return (
-        <>
-        
-          <Swiper
-            spaceBetween={0}
-            centeredSlides={true}
-            autoplay={{
-              delay: 3500,
-              disableOnInteraction: true,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            navigation={true}
-            modules={[Autoplay, Pagination, Navigation]}
-            className="mySwiper"
-          >
-             <SwiperSlide><img src="/home1.png" /></SwiperSlide>      
-             <SwiperSlide><img src="/home2.png" /></SwiperSlide>      
-             <SwiperSlide><img src="https://i.postimg.cc/J42hsCPZ/2-scientist.png" /></SwiperSlide>      
-            <SwiperSlide><img src="https://i.postimg.cc/vZgyyTkp/image.png" /></SwiperSlide>
-            <SwiperSlide><img src="https://i.postimg.cc/0jDVqSGQ/Image-1-00001.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="https://i.postimg.cc/02dwnf6F/nit-patna-002.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="https://i.postimg.cc/zX1hTMfT/nit-patna-003.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="https://i.postimg.cc/h4mxzgKS/nit-patna-004.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="https://i.postimg.cc/fyY06783/nit-patna-005.jpg" /></SwiperSlide>
-            <SwiperSlide><img src="https://i.postimg.cc/sgvVmgT5/Screenshot_2024-07-31_154315.png"/></SwiperSlide>
-            
-            
-          </Swiper>
-        </>
-      );
-}
+  return (
+    <>
+      <Swiper
+        spaceBetween={0}
+        centeredSlides={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://patnapress.com/wp-content/uploads/2024/12/NIT-Bihta.jpeg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/home1.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/home2.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/J42hsCPZ/2-scientist.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/vZgyyTkp/image.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/0jDVqSGQ/Image-1-00001.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/02dwnf6F/nit-patna-002.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/zX1hTMfT/nit-patna-003.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/h4mxzgKS/nit-patna-004.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/fyY06783/nit-patna-005.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.postimg.cc/sgvVmgT5/Screenshot_2024-07-31_154315.png" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
 
-export default Slider
-
+export default Slider;
