f72ff9cddb95eb9ec8d62dc2b2b35e8d9bb8489f
National Insitute of Technology Patna <162422736+nitp-website@users.noreply.github.com>
2025-01-28 16:39:19 +0530

    Update page.js

--- src/app/Department/Phy/mission/page.js
+++ src/app/Department/Phy/mission/page.js
@@ -20,7 +20,7 @@ function Page(){
                 <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md">
                 <div className='text-justify'>
                     <p className="text-red-900 font-semibold lg:text-2xl text-xl">
-                        VISION
+                        Vision
                     </p>
                     <p className="mt-4 lg:text-base text-sm">
                     The vision of the Department is to achieve excellence in undergraduate and postgraduate education and research through a synergic combination and thereby lead to development of new knowledge.
@@ -28,7 +28,7 @@ function Page(){
                 </div>
                 <div className="mt-10 text-justify">
                     <p className="text-red-900 font-semibold lg:text-2xl text-xl">
-                        MISSION
+                        Mission 
                     </p>
                     <p className="mt-4 lg:text-base text-sm">
                     To train the students to be lifelong learners who will contribute to the creation of new knowledge, new technology, and innovation through excellence in research in emerging areas and disciplines.
