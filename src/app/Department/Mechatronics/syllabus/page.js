68a3e40f7b502d3597665d49c8930408c79071aa
Pankaj <pankaj8920858327@gamil.com>
2025-01-31 00:29:08 +0530

    change in mechatronics

--- src/app/Department/Mechatronics/syllabus/page.js
+++ src/app/Department/Mechatronics/syllabus/page.js
@@ -1,9 +1,9 @@
 import BackDepartment from "../../../components/department/BackDepartment";
 
 
-export default function page(){
+export default function page() {
     return (
-        <div className=" flex flex-col p-10 max-sm:p-2 text-black">  
+        <div className=" flex flex-col p-10 max-sm:p-2 text-black">
             <div className="flex flex-col  mb-6">
                 <div>
                     <p className="text-red-900 text-3xl max-sm:text-2xl font-bold">
@@ -12,45 +12,40 @@ export default function page(){
                 </div>
                 <div className="mt-2">
                     <p className="text-gray-500 text-base font-semibold">
-                    Mechatronics & Automation
+                        Mechatronics and Automation Engineering
                     </p>
                 </div>
-                <BackDepartment navigate={"/Department/Mechatronics"}/>
+                <BackDepartment navigate={"/Department/Mechatronics"} />
 
             </div>
 
-            <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md">
-                {/* UG PROGRAM */}
-                <div className="mb-10">
-                    <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">UG PROGRAM :-</div>
-                    {/* <ul className="list-disc list-inside ml-5 space-y-4 text-lg max-md:text-[15px] max-sm:text-sm">
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1A0l-C3vXUjL3qrV6dZutObFlQKiVhOuJ/view">B.Tech_EE_Course Structure_Batch-2021</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1F6i7jfE9LzhGJM7M1SqbDhtDXd8dq9Dj/view?usp=drive_link">B.Tech_Detailed Syllabus_Batch-2021</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/198DYWz6y1TK1lEeNX_V9eP01aNamRJtW/view">B.Tech_EE_Course Structure_Batch-2022</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1FVrOsNGJdN09TRtKnvb9_rALJy8E5Dwz/view">B.Tech_Detailed Syllabus_Batch-2022</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1gDzCfUQfjaUqFNP61z3YG-R6H_8K6y89/view">B.Tech_EE_Course Structure_Batch-2023_Onwards</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/131yWvyK8E7ZIju7abxcT76D3NfYXHvqR/view">B.Tech_Detailed Syllabus_Batch-2023_Onwards</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1R24uKKW9fxbtv6TlWzCfgIuqTydIN5ND/view">Dual Degree (B.Tech + M.Tech (PSE))_Course Structure_Batch-2022</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1PlIaW0QziCXjOoMyy425vZocUJn9geuO/view">Dual Degree (B.Tech + M.Tech (PSE))_Detailed Syllabus_Batch-2022</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1D-JX5uoJ3Vf7tENlyq0Z3AEQKAAinfBo/view">Dual Degree (B.Tech + M.Tech (PSE))_Course Structure_Batch-2023_Onwards</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1LJHJqsRSC8-V_P3zkAxu6wn5mdyWfu7m/view">Dual Degree (B.Tech + M.Tech (PSE))_Detailed Syllabus_Batch-2023_Onwards</a></li>
-                    </ul> */}
+            <div className="container mx-auto mt-3 bg-red-100  w-[70%] rounded-xl pt-4">
+            <div className="container mx-auto px-4  bg-red-100 text-black w-full md:w-[70%]">
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-8">
+                    <div className="flex items-center">
+                        <p className="text-xs sm:text-base ">B.Tech Mechatronics and Automation Engineering :</p>
+                    </div>
+                    <div className="flex justify-end">
+                        <a href="https://drive.google.com/file/d/1IQFqpo9jTHIBNgS8o2zeyv5xIZYWcQ7p/view?usp=sharing">
+                            <button className="bg-red-700 hover:bg-red-600 text-white p-1 px-2 rounded text-xs sm:text-sm">
+                                Course Structure & Syllabus
+                            </button>
+                        </a>
+                    </div>
                 </div>
-                {/* PG PROGRAM  */}
-                <div className="mb-10">
-                    <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">PG PROGRAM :-</div>
-                    {/* <ul className="list-disc list-inside ml-5 space-y-4 text-lg max-md:text-[15px] max-sm:text-sm">
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1HOzQYLI-96kpv4gT9NDoKqLItHsALVzS/view">M.Tech- Control System Engg_and_Power System Engg_Course structure</a></li>
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1xnaep1qftT-n8RMVEyTYXk-ASqBFcLAK/view">M.Tech- Control System Engg_and_Power System Engg_Detailed Syllabus</a></li>
-                    </ul> */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-8">
+                    <div className="flex items-center">
+                        <p className="text-xs sm:text-base ">B.Tech Mechatronics and Automation Engineering :</p>
+                    </div>
+                    <div className="flex justify-end">
+                        <a href="https://drive.google.com/file/d/107lkKUTm9Yoilr_RiP2BpnkAy_VU1soJ/view?usp=sharing">
+                            <button className="bg-red-700 hover:bg-red-600 text-white p-1 px-2 rounded text-xs sm:text-sm">
+                                Course Structure 
+                            </button>
+                        </a>
+                    </div>
                 </div>
-                {/* PHD PROGRAM  */}
-                <div className="mb-10">
-                    <div className="text-red-900 font-semibold lg:text-2xl text-xl mb-8">PHD PROGRAM :-</div>
-                    {/* <ul className="list-disc list-inside ml-5 space-y-4 text-lg max-md:text-[15px] max-sm:text-sm">
-                        <li className="hover:text-blue-800 text-sm lg:text-base"><a href="https://drive.google.com/file/d/1KdqphtQwIuWrC_gdYxjuAcG9TaxaUVf-/view">Ph.D. in Electrical Engineering</a></li>
-                    </ul> */}
-                </div>
+            </div>
             </div>
         </div>
     )
--- src/app/Department/Mechatronics/timeTable/page.js
+++ src/app/Department/Mechatronics/timeTable/page.js
@@ -1,10 +1,17 @@
-
-
 import BackDepartment from "../../../components/department/BackDepartment"
 
-function Page(){
+
+function Page() {
+    const data = [
+        {
+            name: "Mechatronics and Automation Engineering  Dept. Time Table for the Class Jan- June 2025 (Even Semster)",
+            link: "https://drive.google.com/file/d/1hpZ6RVveUhFo-noguVNRulif-BplAZP4/view?usp=sharing"
+        },
+
+
+    ];
     return (
-        <div className=" flex flex-col p-5 h-screen text-black">  
+        <div className=" flex flex-col p-5 h-screen text-black">
             <div className="flex flex-col md:ml-10">
                 <div>
                     <p className="text-red-900 text-3xl font-bold">
@@ -13,19 +20,35 @@ function Page(){
                 </div>
                 <div className="mt-2">
                     <p className="text-gray-500 text-xl font-semibold">
-                        Mechatronics & Automation
+                        Mechatronics and Automation Engineering
                     </p>
                 </div>
                 <div>
-                <BackDepartment navigate={'/Department/Mechatronics'}/>
+                    <BackDepartment navigate={'/Department/Mechatronics'} />
                 </div>
             </div>
-            <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md h-screen">
-                <div className='mt-14 '>
-                    {/* <a href={'https://www.dropbox.com/scl/fo/kmdu6u1erwn18lwo0qe4p/h?rlkey=rl1xo6nzihg0j5yc9zvfidfr0&dl=0'} className='lg:text-lg text-gray-500 font-semibold border-b-2 border-black hover:text-blue-500' target="_blank">
-                        Click here for the time table of CSED (for all semesters)
-                    </a> */}
-                </div>
+            <div className="md:mx-auto px-4 py-8 w-4/5">
+
+                <table className="w-full mx-auto border-collapse border border-neutral-600 bg-white rounded-lg">
+                    <thead>
+                        <tr>
+                            <th className="text-left px-2 py-2 md:px-4 md:py-4 bg-gradient-to-r from-red-200 to-pink-200 text-black">Name</th>
+                            <th className="text-center px-2 py-2 md:px-4 md:py-4 bg-gradient-to-r from-red-200 to-orange-200 text-black">Download Now</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((item, index) => (
+                            <tr key={index}>
+                                <td className="text-xs md:text-base text-left px-2 md:px-4 py-2 border-b border-gray-300 text-black">{item.name}</td>
+                                <td className="text-xs md:text-base text-center px-4 py-3 border-b border-gray-300">
+                                    <a href={item.link} className="bg-blue-400 text-white font-bold px-4 py-2 rounded-md hover:bg-gradient-to-r from-cyan-500 to-blue-500 md:text-xs text-xs" target="_blank">
+                                        Download
+                                    </a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
 
