ff297f0827f796ea7ef9a250461ae6886e6264bf
aman <amanm.ug23.cs@nitp.ac.in>
2025-02-18 23:23:14 +0530

    Updated HOD page same data

--- src/app/Others/NIRF/page.js
+++ src/app/Others/NIRF/page.js
@@ -1,119 +1,31 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Page = () => {
   const [data, setData] = useState([]);
 
-  const dataSets = {
-    first: [
+  useEffect(() => {
+    setData([
       {
-        para: "Annual Report 2019-20(Hindi Version)",
-        link:
-          "https://drive.google.com/file/d/143anlPYa2GdEsRDNT6APGEX7RXYUgXuK/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2019-20(English Version)",
-        link:
-          "https://drive.google.com/file/d/1MnuYPPEln2UNVeYNDS137Bd9uAks6_Q-/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2018-19(Hindi Version)",
-        link:
-          "https://drive.google.com/file/d/1ibIlfkbI8tScyCfxuokrjVKYfVhr9g1S/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2018-19(English Version)",
-        link:
-          "https://drive.google.com/file/d/1CgoyEykM4YkqMMK-WbvuWfvGyQ3i_6JM/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2017-18",
-        link:
-          "https://drive.google.com/file/d/1IDk_1u0nLQw6cNBGk51aynWuXZhq4Tsb/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2016-17",
-        link:
-          "https://drive.google.com/file/d/1hz-gKnduzkMo_H8Gl-F9GMTJ9bj842d3/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2015-16",
-        link:
-          "https://drive.google.com/file/d/1yk0zhlx6E_jrVadEg_La_n8z_FRVsDF7/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2014-15",
-        link:
-          "https://drive.google.com/file/d/14ssl9RbPeXntGANv7m5UY1D4y5Yxlshz/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2013-14",
-        link:
-          "https://drive.google.com/file/d/1rKl-x5GAmWGADz2U7OvU1T5UMIaGC7jB/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2012-13",
-        link:
-          "https://drive.google.com/file/d/12ucyxJAIfwC1B3mGBHz95CUmXBeWGDlX/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2011-12",
-        link:
-          "https://drive.google.com/file/d/1MXCJJXeCI_kVc_MkveUieZPrrBuJneWg/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2010-11",
-        link:
-          "https://drive.google.com/file/d/1ILUTChm2aZ_Y45dJkllizd5IPNtH_oL8/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2009-10",
-        link:
-          "https://drive.google.com/file/d/1FoJy3nITB3j5SBzCIYTuN9H6ngK6NN-k/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2008-09",
-        link:
-          "https://drive.google.com/file/d/1Od5lXthly7izI7d_OGiL8HltmyflA4iY/view?usp=drive_link",
-      },
-      {
-        para: "Annual Report 2007-08",
-        link:
-          "https://drive.google.com/file/d/1BiQszH1Z_x7ntx8Jw0GiQJrmW1ofYGei/view?usp=drive_link",
-      },
-    ],
-    second: [
-      {
-        para: "NIRF Data 2025-26",
+        para: "NIRF Data 2024-25",
         link: "https://drive.google.com/file/d/1SLsT-sa8sSynXtTQ-jMR5seoIsMT1Vft/view?usp=sharing",
       },
       {
-        para: "NIRF Data 2024-25",
+        para: "NIRF Data 2023-24",
         link: "https://drive.google.com/file/d/1F85taARFc0uM-NX4Dg8CIbe8m9HwcRi1/view",
       },
-      // {
-      //   para: "NIRF Data 2023-24",
-      //   link: "https://drive.google.com/file/d/1Jobn8OBYPUf1ekffjTS0ZIzXuzfQ8CNa/view?usp=sharing",
-      // },
-    ],
-    third: [
-      { para: "Functions and Duties", link: "#" },
-      { para: "Powers and duties of its officers and employees", link: "#" },
-      { para: "The procedure followed in the decision making process, including channels of supervision and accountability", link: "#" },
-      { para: "The norms set by it for the discharge of its functions", link: "#" },
-      { para: "	The rules, regulations, instructions, manuals and records, held by it or under its control or used by its employees for discharging its functions", link: "#" },
-    ],
-  };
+    ]);
+  }, []);
 
   return (
     <div className="text-center text-black mt-14">
       <h1 className="text-3xl font-extrabold text-red-900">NIRF Data</h1>
 
-      <h3 className="text-xl pt-8 text-red-800 font-bold">
+      {/* <h3 className="text-xl pt-8 text-red-800 font-bold">
         <span className="text-lg text-neutral-700 pb-5">Coordinator :</span> Prof. Prakash Chandra
-      </h3>
-      <h4 className="text-lg text-neutral-700 pb-5">
+      </h3> */}
+      {/* <h4 className="text-lg text-neutral-700 pb-5">
         <span className="text-lg text-neutral-700 pb-5"> Email:</span>
         <a
           href="mailto:prakash@nitp.ac.in"
@@ -123,34 +35,10 @@ const Page = () => {
         >
           prakash@nitp.ac.in
         </a>
-
-
-      </h4>
-
-      <div className="flex flex-col md:flex-row md:space-x-20 space-y-4 md:space-y-0 mb-6 justify-center mx-4 md:mx-0">
-        <button
-          className="px-6 py-3 bg-[#a12c23] text-white rounded-lg shadow-lg hover:bg-[#a0002b] transition-transform transform hover:rounded-full duration-500 duration-1700 ease-in-out"
-          onClick={() => setData(dataSets.first)}
-        >
-          Annual Report
-        </button>
-        <button
-          className="px-6 py-3 bg-[#a12c23] text-white rounded-lg shadow-lg hover:bg-[#a0002b] transition-transform transform hover:rounded-full duration-500 duration-700 ease-in-out"
-          onClick={() => setData(dataSets.second)}
-        >
-          NIRF
-        </button>
-        <button
-          className="px-6 py-3 bg-[#a12c23] text-white rounded-lg shadow-lg hover:bg-[#a0002b] transition-transform transform hover:rounded-full duration-500 duration-700 ease-in-out"
-          onClick={() => setData(dataSets.third)}
-        >
-          Act and Statutes
-        </button>
-      </div>
+      </h4> */}
 
       <div className="w-full p-4 border border-gray-300 rounded-lg shadow-md bg-white flex justify-center">
         <div className="w-full max-w-4xl">
-
           <table className="w-full border-collapse border border-neutral-600 bg-white rounded-lg p-0 m-0 mt-4">
             <thead>
               <tr>
