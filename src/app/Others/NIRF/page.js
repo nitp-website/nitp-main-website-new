389b82661ce06279a57885e4c678763a2fe6305e
aman <amanm.ug23.cs@nitp.ac.in>
2025-02-18 22:45:13 +0530

    Others/NIRF updated

--- src/app/Others/NIRF/page.js
+++ src/app/Others/NIRF/page.js
@@ -1,22 +1,186 @@
-import React from 'react'
+"use client";
 
-const page = () => {
+import React, { useState } from "react";
+
+const Page = () => {
+  const [data, setData] = useState([]);
+
+  const dataSets = {
+    first: [
+      {
+        para: "Annual Report 2019-20(Hindi Version)",
+        link:
+          "https://drive.google.com/file/d/143anlPYa2GdEsRDNT6APGEX7RXYUgXuK/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2019-20(English Version)",
+        link:
+          "https://drive.google.com/file/d/1MnuYPPEln2UNVeYNDS137Bd9uAks6_Q-/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2018-19(Hindi Version)",
+        link:
+          "https://drive.google.com/file/d/1ibIlfkbI8tScyCfxuokrjVKYfVhr9g1S/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2018-19(English Version)",
+        link:
+          "https://drive.google.com/file/d/1CgoyEykM4YkqMMK-WbvuWfvGyQ3i_6JM/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2017-18",
+        link:
+          "https://drive.google.com/file/d/1IDk_1u0nLQw6cNBGk51aynWuXZhq4Tsb/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2016-17",
+        link:
+          "https://drive.google.com/file/d/1hz-gKnduzkMo_H8Gl-F9GMTJ9bj842d3/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2015-16",
+        link:
+          "https://drive.google.com/file/d/1yk0zhlx6E_jrVadEg_La_n8z_FRVsDF7/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2014-15",
+        link:
+          "https://drive.google.com/file/d/14ssl9RbPeXntGANv7m5UY1D4y5Yxlshz/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2013-14",
+        link:
+          "https://drive.google.com/file/d/1rKl-x5GAmWGADz2U7OvU1T5UMIaGC7jB/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2012-13",
+        link:
+          "https://drive.google.com/file/d/12ucyxJAIfwC1B3mGBHz95CUmXBeWGDlX/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2011-12",
+        link:
+          "https://drive.google.com/file/d/1MXCJJXeCI_kVc_MkveUieZPrrBuJneWg/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2010-11",
+        link:
+          "https://drive.google.com/file/d/1ILUTChm2aZ_Y45dJkllizd5IPNtH_oL8/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2009-10",
+        link:
+          "https://drive.google.com/file/d/1FoJy3nITB3j5SBzCIYTuN9H6ngK6NN-k/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2008-09",
+        link:
+          "https://drive.google.com/file/d/1Od5lXthly7izI7d_OGiL8HltmyflA4iY/view?usp=drive_link",
+      },
+      {
+        para: "Annual Report 2007-08",
+        link:
+          "https://drive.google.com/file/d/1BiQszH1Z_x7ntx8Jw0GiQJrmW1ofYGei/view?usp=drive_link",
+      },
+    ],
+    second: [
+      {
+        para: "NIRF Data 2025-26",
+        link: "https://drive.google.com/file/d/1SLsT-sa8sSynXtTQ-jMR5seoIsMT1Vft/view?usp=sharing",
+      },
+      {
+        para: "NIRF Data 2024-25",
+        link: "https://drive.google.com/file/d/1F85taARFc0uM-NX4Dg8CIbe8m9HwcRi1/view",
+      },
+      // {
+      //   para: "NIRF Data 2023-24",
+      //   link: "https://drive.google.com/file/d/1Jobn8OBYPUf1ekffjTS0ZIzXuzfQ8CNa/view?usp=sharing",
+      // },
+    ],
+    third: [
+      { para: "Functions and Duties", link: "#" },
+      { para: "Powers and duties of its officers and employees", link: "#" },
+      { para: "The procedure followed in the decision making process, including channels of supervision and accountability", link: "#" },
+      { para: "The norms set by it for the discharge of its functions", link: "#" },
+      { para: "	The rules, regulations, instructions, manuals and records, held by it or under its control or used by its employees for discharging its functions", link: "#" },
+    ],
+  };
+
   return (
-    <div className='text-center text-black mt-14'>
-        <h1 className='text-3xl font-extrabold text-red-900'>NIRF Data</h1>
+    <div className="text-center text-black mt-14">
+      <h1 className="text-3xl font-extrabold text-red-900">NIRF Data</h1>
 
-        <h3 className='text-xl pt-8 text-red-800 font-bold' >Coordinator : Prof. Prakash Chandra </h3>
-        <h4 className='text-lg text-neutral-700 pb-5'>email : <a href="https://mail.google.com/mail/?view=cm&to=prakash@nitp.ac.in" target="_blank">prakash@nitp.ac.in</a></h4>
-        <a href="https://drive.google.com/file/d/1F85taARFc0uM-NX4Dg8CIbe8m9HwcRi1/view" target="_blank" >
-        <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white  focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0 ">
-        NIRF 2024
-        </span>
-        </button>
+      <h3 className="text-xl pt-8 text-red-800 font-bold">
+        <span className="text-lg text-neutral-700 pb-5">Coordinator :</span> Prof. Prakash Chandra
+      </h3>
+      <h4 className="text-lg text-neutral-700 pb-5">
+        <span className="text-lg text-neutral-700 pb-5"> Email:</span>
+        <a
+          href="mailto:prakash@nitp.ac.in"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-xl pt-8 text-red-800 font-bold transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+        >
+          prakash@nitp.ac.in
         </a>
-        
-    </div>
-  )
-}
 
-export default page
+
+      </h4>
+
+      <div className="flex flex-col md:flex-row md:space-x-20 space-y-4 md:space-y-0 mb-6 justify-center mx-4 md:mx-0">
+        <button
+          className="px-6 py-3 bg-[#a12c23] text-white rounded-lg shadow-lg hover:bg-[#a0002b] transition-transform transform hover:rounded-full duration-500 duration-1700 ease-in-out"
+          onClick={() => setData(dataSets.first)}
+        >
+          Annual Report
+        </button>
+        <button
+          className="px-6 py-3 bg-[#a12c23] text-white rounded-lg shadow-lg hover:bg-[#a0002b] transition-transform transform hover:rounded-full duration-500 duration-700 ease-in-out"
+          onClick={() => setData(dataSets.second)}
+        >
+          NIRF
+        </button>
+        <button
+          className="px-6 py-3 bg-[#a12c23] text-white rounded-lg shadow-lg hover:bg-[#a0002b] transition-transform transform hover:rounded-full duration-500 duration-700 ease-in-out"
+          onClick={() => setData(dataSets.third)}
+        >
+          Act and Statutes
+        </button>
+      </div>
+
+      <div className="w-full p-4 border border-gray-300 rounded-lg shadow-md bg-white flex justify-center">
+        <div className="w-full max-w-4xl">
+
+          <table className="w-full border-collapse border border-neutral-600 bg-white rounded-lg p-0 m-0 mt-4">
+            <thead>
+              <tr>
+                <th className="text-left px-4 py-4 bg-gradient-to-r from-blue-200 to-cyan-200 text-black">Document</th>
+                <th className="text-center w-[10vw] px-4 py-4 bg-gradient-to-r from-blue-200 to-cyan-200 text-black">Download Now</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 ? (
+                data.map((item, index) => (
+                  <tr key={index}>
+                    <td className="text-left px-4 py-2 border-b border-gray-300 text-black">{item.para}</td>
+                    <td className="text-center px-4 py-3 border-b border-gray-300">
+                      <a href={item.link} className="bg-blue-400 text-white font-bold px-4 py-2 rounded-md hover:bg-gradient-to-r from-cyan-500 to-blue-500 md:text-xs text-xs">
+                        Download
+                      </a>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="2" className="text-center px-4 py-3 border-b border-gray-300 text-gray-500">No data available</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
