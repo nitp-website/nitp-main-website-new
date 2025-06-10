f08f6e948b231f9aaf8b32d9179b0c1cec00a4d2
National Insitute of Technology Patna <162422736+nitp-website@users.noreply.github.com>
2025-06-10 21:19:53 +0530

    Update page.jsx

--- src/app/Academic/NITPAdmission/page.jsx
+++ src/app/Academic/NITPAdmission/page.jsx
@@ -85,7 +85,7 @@ const admissionData = {
         name: "NIMCET",
         image: "https://cdnbbsr.s3waas.gov.in/s33e6260b81898beacda3d16db379ed329/uploads/2025/03/2025031961.png",
         link: "https://nimcet.admissions.nic.in/",
-        description: "Centralized Counselling for M.C.A. Admissions"
+        description: "Centralized Counselling for MCA Admissions"
       },
     ],
     notices: [
@@ -209,7 +209,7 @@ function AdmissionsPage() {
             >
               <span className="flex items-center">
                 <GraduationCap className="w-5 h-5 mr-3" />
-                M.C.A Admissions
+                MCA Admissions
               </span>
             </li>
 
