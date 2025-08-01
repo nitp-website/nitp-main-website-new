bd64898442104a0f5624d7dd9819a986f36160ad
Aashish Kumar Singh <aashishs.ug23.cs@nitp.ac.in>
2025-08-01 07:57:45 +0530

    updated staff details for chemical science and technology

--- src/app/Department/staffChemical.js
+++ src/app/Department/staffChemical.js
@@ -3,6 +3,28 @@ const staffData = [
         department: "Chemical",
         staff: [
             {
+                name: "Mr. Aditya Kumar Bharti",
+                designation: "Technical Assistant",
+                experience: "Technician at NIT Surat (April 2023 to August 2023)",
+                education: "MSc",
+                expertise: "N/A",
+                dateOfJoining: "Sep 08, 2023",
+                email: "aditya.ta@nitp.ac.in",
+                mobile: "0612-2371540",
+                image: "https://i.postimg.cc/V64g7ypj/image.png",
+            },
+            {
+                name: "Mr. Vishnu Shankar Rai",
+                designation: "Technical Assistant",
+                experience: "NA",
+                education: "M.Sc.",
+                expertise: "Physical Chemistry",
+                dateOfJoining: "03/07/2024",
+                email: "vishnu.ta@nitp.ac.in",
+                mobile: "N/A",
+                image: "https://i.postimg.cc/fW25mNxs/image.png",
+            },
+            {
                 name: "Mr. Debabrata De",
                 designation: "Technician",
                 experience: "Worked as NMR operator at IIT Patna for 1.6 years",
@@ -12,7 +34,6 @@ const staffData = [
                 mobile: "+91-",
                 image: "/staffChemical.png",
             },
-            
         ],
     },
 ];
