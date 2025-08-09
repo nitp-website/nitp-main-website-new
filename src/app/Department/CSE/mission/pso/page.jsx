3eb6e1b171efd497fc68f54b66228e330255ee02
Aashish Kumar Singh <aashishs.ug23.cs@nitp.ac.in>
2025-08-09 19:44:27 +0530

    Add Program Outcomes and Programme Educational Objectives pages with navigation buttons

---
+++ src/app/Department/CSE/mission/pso/page.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Comp } from "../page";
+
+function page() {
+  return (
+    <div className="flex flex-col p-5 text-black">
+      <div className="mt-10">
+        <p className="text-red-900 text-xl lg:text-2xl font-semibold">
+          Program Specific Outcome
+        </p>
+        <Comp
+          content={[
+            "PSO1: Analyze, design, model, develop, test, and manage complex software and information management systems.",
+            "PSO2: Develop sustainable, intelligent, and secure computing solutions for real time challenges.",
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default page;
---
+++ src/app/Department/Chem/peo/page.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Comp } from "../mission/page";
+function page() {
+  return (
+    <div className="flex flex-col p-5 text-black">
+      <div className="mt-10">
+        <p className="text-red-900 font-semibold lg:text-2xl text-xl">
+          PEO (Programme Educational Objectives)
+        </p>
+        <p>
+          Within a few years of graduating from the B.Tech.+M.Tech Dual Degree
+          program in Chemical Technology, graduates will be able to:
+        </p>
+        <Comp
+          content={[
+            "PEO1: Apply foundational knowledge and experimental expertise in basic sciences, mathematics, and core chemical engineering to solve real-world challenges in chemical and related industries.",
+            "PEO2: Plan and execute chemical engineering projects aimed at enhancing quality of life.",
+            "PEO3: Critically analyze and address issues concerning safety, energy efficiency, and environmental sustainability.",
+            "PEO4: Exhibit strong communication, project management, and leadership capabilities in professional settings.",
+            "PEO5: Perform comprehensive life cycle analysis (cradle-to-grave) of chemical processes to promote sustainable engineering practices.",
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default page;
