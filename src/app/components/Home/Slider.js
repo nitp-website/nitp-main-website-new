b07a5f1f4967154f76c097fa75574f6a54b0b202
Aashish Kumar Singh <aashishs.ug23.cs@nitp.ac.in>
2025-10-13 18:53:13 +0530

    fix(Slider): update image source for a SwiperSlide

--- src/app/components/Home/Slider.js
+++ src/app/components/Home/Slider.js
@@ -34,7 +34,7 @@ const Slider = () => {
           <img src="https://i.postimg.cc/0N1Yfkhc/NIRF.png" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://nitp-database-s3.s3.ap-south-1.amazonaws.com/Top+2%25+final.webp" />
+          <img src="https://i.postimg.cc/Jh8Ffb9h/image.png" />
         </SwiperSlide>
         <SwiperSlide>
           <img src="https://i.postimg.cc/FHQKvKyk/image.png" />
