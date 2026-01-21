f0885364b523cbbc013861a02f26992cfca0a70e
Aashish Kumar Singh <aashishs.ug23.cs@nitp.ac.in>
2026-01-21 08:48:16 +0530

    chemistry staff details updated

---
+++ src/app/Department/staffChem.js
@@ -0,0 +1,49 @@
+const staffData = [
+    {
+        department: "CHEM",
+        staff: [
+            {
+                name: "Mr. Aditya Kumar Bharti",
+                designation: "Technical Assistant",
+                experience: "Technician at NIT Surat (April 2023 to August 2023)",
+                education: "MSc",
+                // expertise: "N/A",
+                email: "aditya.ta@nitp.ac.in",
+                mobile: "0612-2371540",
+                image: "https://i.postimg.cc/0j7F6CBC/image.png",
+            },
+            {
+                name: "Mr. Vishnu Shankar Rai",
+                designation: "Technical Assistant",
+                experience: "N/A",
+                education: "M.Sc.",
+                expertise: "Physical Chemistry",
+                email: "vishnu.ta@nitp.ac.in",
+                mobile: "",
+                image: "https://i.postimg.cc/LshWkSbw/image.png",
+            },
+            {
+                name: "Mr. Debabrata De",
+                designation: "Technician",
+                experience: "JEOL 500 MHz NMR Operator at IIT Patna (1st Sep. 2022 to 31st Jan. 2024)",
+                education: "MSc",
+                expertise: "Organic Chemistry",
+                email: "debabrata.tech@nitp.ac.in",
+                mobile: "+91-9002434037",
+                image: "https://i.postimg.cc/QCZnrTdC/image.png",
+            },
+            {
+                name: "Mr. Basudeo Lal",
+                designation: "Office Attendant (SG-II)",
+                experience: "35 Years",
+                education: "Matric",
+                expertise: "Administration",
+                email: "basudeo.ch@nitp.ac.in",
+                mobile: "0612-2371540",
+                image: "https://i.postimg.cc/T33NGGfS/image.png",
+            },
+        ],
+    },
+];
+
+export default staffData;
