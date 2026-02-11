0e7d6fde6404038ac1cdf999eff090e2e5841f89
Aashish Kumar Singh <aashishs.ug23.cs@nitp.ac.in>
2026-02-11 19:37:10 +0530

    replacecd outdated images

--- src/app/components/global/Imagefooter.js
+++ src/app/components/global/Imagefooter.js
@@ -1,9 +1,9 @@
 import React from 'react'
 function Imagefooter() {
     return (
-        <footer className="w-full h-[300px] md:h-[90vh] bg-cover bg-no-repeat bg-center relative scroll-mt-32 py-8 transition-all duration-300 ease-in-out"
+        <footer className="w-full h-[300px] md:h-[110vh] bg-cover bg-no-repeat bg-center relative scroll-mt-32 py-8 transition-all duration-300 ease-in-out"
             style={{
-                backgroundImage: "linear-gradient(to bottom, white, rgba(0, 0, 0, 0.1)), url('https://nitp-database-s3.s3.ap-south-1.amazonaws.com/MainEntrance.webp')"
+                backgroundImage: "linear-gradient(to bottom, white, rgba(0, 0, 0, 0.1)), url('https://i.postimg.cc/2SG1CvT3/dji-fly-20260211-142624-554-1770809717334-photo(1)-jpg.jpg')"
             }}
         ></footer>
     )
