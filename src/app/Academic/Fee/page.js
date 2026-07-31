7f1c88bed83635ffae9d2514770eee80f768604a
National Insitute of Technology Patna | Web Development Cell <162422736+nitp-website@users.noreply.github.com>
2026-07-31 16:59:02 +0530

    Update page.js

--- src/app/Academic/Fee/page.js
+++ src/app/Academic/Fee/page.js
@@ -5,6 +5,8 @@ const FeeStructureTable = () => {
   const PhD = [
     { name: 'Fee Structure 2023-24 Onwords', 
       link: 'https://drive.google.com/file/d/1b6bsHkVHfPa0gsG3Z9vhUKrsrGb3Sw4h/view' },
+    { name: 'Fee Structure 2026-27 Onwords',
+      link: 'https://drive.google.com/file/d/1n6AkZ0K_TXuSkzbCbvzUb8ucXV3omR5Y/view?usp=sharing' },
      ];
    const MCA = [
      { name: 'Fee Structure for MCA  Regular 2023-24 onwards.',
