fd40442e35e9498fd49a959091051c8d1a5390dd
Lalit gurjar <gujarlalit79@gmail.com>
2026-08-11 23:10:10 +0530

    ece academic program updated

--- src/app/Department/ECE/acadProgram/AcadProgramCard.jsx
+++ src/app/Department/ECE/acadProgram/AcadProgramCard.jsx
@@ -3,65 +3,73 @@ import Link from "next/link";
 
 export const AcadProgramCard = ({ program }) => {
   return (
-    <div className="w-full mx-auto md:w-[48%] bg-white border-2 border-red-800 rounded-xl shadow transition-shadow p-2 md:p-6 flex flex-col justify-between mb-4 hover:shadow-lg shadow-gray-200">
-      <div>
-        {
-          program.title && (
-            <h3 className="text-xl font-semibold text-start text-red-800 mb-2">
+    <div className="w-full px-2 mb-4 md:w-1/2">
+      <div className="h-full bg-white border-2 border-red-800 rounded-xl shadow transition-shadow p-3 md:p-6 flex flex-col justify-between hover:shadow-lg shadow-gray-200">
+        <div>
+          {program.title && (
+            <h3 className="text-lg sm:text-xl font-semibold text-start text-red-800 mb-2">
               {program.title}
             </h3>
-          )
-        }
-        {
-          program.degree && (
+          )}
+          {program.degree && (
             <p className="text-base text-gray-600 mb-1">
               <strong>Degree:</strong>{" "}
               <span className="font-semibold text-black">{program.degree}</span>
             </p>
-          )
-        }
-        {
-          program.duration && (
+          )}
+          {program.duration && (
             <p className="text-sm text-gray-600 mb-3">
               <strong>Duration:</strong>{" "}
-              <span className="font-semibold text-black">{program.duration}</span>
+              <span className="font-semibold text-black">
+                {program.duration}
+              </span>
             </p>
-          )
-        }
-        {
-          program.description && (
-            <p className="text-gray-700 text-base text-justify">
+          )}
+          {program.description && (
+            <p className="text-gray-700 text-sm sm:text-base text-justify">
               {program.description}
             </p>
-          )
-        }
-      </div>
-      <div className="mt-4 mx-auto flex gap-4 justify-between items-center flex-wrap">
-        {program.syllabus && (
-          <a target="_blank" href={`${program.syllabus}`} >
-            <button className="text-red-800 border border-red-800 px-4 py-1 text-sm rounded-md  transition hover:bg-red-800 hover:text-white cursor-pointer">
-              Curriculum / Syllabus
-            </button>
-          </a>
-        )}
-
-        {program.courseStructure && (
-          <a target="_blank" href={`${program.courseStructure}`} >
-            <button className="text-red-800 border border-red-800 px-4 py-1 text-sm rounded-md  transition hover:bg-red-800 hover:text-white cursor-pointer">
-              Course Structure
-            </button>
-          </a>
-        )}
-
-        {
-          program.from && program.from.map((item, index) => (
-            <a key={index} target="_blank" href={`${item.courseStructure}`} >
-              <button className="text-red-800 border border-red-800 px-4 py-1 text-sm rounded-md  transition hover:bg-red-800 hover:text-white cursor-pointer mt-2">
-                Curriculum {item.time}
+          )}
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2 justify-center md:justify-between items-center">
+          {program.syllabus && (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`${program.syllabus}`}
+            >
+              <button className="w-full sm:w-auto text-red-800 border border-red-800 px-3 py-2 text-sm rounded-md transition hover:bg-red-800 hover:text-white cursor-pointer">
+                Curriculum / Syllabus
               </button>
             </a>
-          ))
-        }
+          )}
+
+          {program.courseStructure && (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={`${program.courseStructure}`}
+            >
+              <button className="w-full sm:w-auto text-red-800 border border-red-800 px-3 py-2 text-sm rounded-md transition hover:bg-red-800 hover:text-white cursor-pointer">
+                Course Structure
+              </button>
+            </a>
+          )}
+
+          {program.from &&
+            program.from.map((item, index) => (
+              <a
+                key={index}
+                target="_blank"
+                rel="noopener noreferrer"
+                href={`${item.courseStructure}`}
+              >
+                <button className="w-full sm:w-auto text-red-800 border border-red-800 px-3 py-2 text-sm rounded-md transition hover:bg-red-800 hover:text-white cursor-pointer">
+                  Curriculum {item.time}
+                </button>
+              </a>
+            ))}
+        </div>
       </div>
     </div>
   );
--- src/app/Department/ECE/acadProgram/page.js
+++ src/app/Department/ECE/acadProgram/page.js
@@ -27,8 +27,13 @@ const programs = [
             "https://drive.google.com/file/d/1rfCcCrPd9mlA8rXhDOWpxhsItBcgIC-G/view",
         },
         {
+          time: "2024 Admitted",
+          courseStructure:
+            "https://drive.google.com/file/d/1u_cwi0J_VEst8gKFEScSWkAEvb3ReK_m/view?usp=drive_link",
+        },
+        {
           time: "2025 Onwards",
-          courseStructure: "https://drive.google.com/file/d/1A6ler7Z8ant1fD4mRBGUes0L3ajYmvy6/view"
+          courseStructure: "https://drive.google.com/file/d/1zkaB-1USaHeaIco0Yl1wirYaaBT09Kk9/view?usp=drive_link"
         }
       ]
   },
@@ -39,7 +44,7 @@ const programs = [
     description:
       "The admission to the 4-year Electronics Engineering (VLSI Design and Technology) is through prestigious IIT-JEE (Main). The programme includes a set of core courses together with few electives so that the students can specialize their interests. Following are the course structure and syllabus.",
     syllabus:
-      "https://drive.google.com/file/d/1UnG1AgDC2E9JSyAFw1v91BHCYOhJcSrO/view",
+      "https://drive.google.com/file/d/11N0GC9s753eE-v0Nh7GNP8rG_AWSny-h/view?usp=drive_link",
   },
   {
     title:
@@ -56,9 +61,14 @@ const programs = [
             "https://drive.google.com/file/d/1dRXVrtFZF0DDqiWKQKMWrL6rpGaSK5H7/view?usp=sharing",
         },
         {
+          time: "2024 Admitted",
+          courseStructure:
+            "https://drive.google.com/file/d/1OIOYPJOIwUnJpDPyGBnhPz3eCnFg_eJa/view?usp=drive_link",
+        },
+        {
           time: "2025 Onwards",
-          courseStructure: 
-            "https://drive.google.com/file/d/1YR9LVVXEwXMKfk47YJOMIx5xvqxhco95/view?usp=sharing"
+          courseStructure:
+            "https://drive.google.com/file/d/1n4jqyGS4HhyfiDdLraZXApY-8Z5yVicx/view?usp=drive_link"
         }
       ]
   },
@@ -110,13 +120,13 @@ const programs = [
 const ECEAcadProgrampage = () => {
   return (
     <div>
-      <div className="py-1 mt-2">
-        <div className="xs:px-0 w-[90%] mx-auto">
+      <div className="py-4 mt-2">
+        <div className="w-full max-w-6xl px-4 sm:px-6 lg:px-8 mx-auto">
           <div className="w-full">
-            <h2 className="text-center text-4xl text-red-700 mt-2">
+            <h2 className="text-center text-2xl sm:text-3xl md:text-4xl text-red-700 mt-2">
               Academic Programs
             </h2>
-            <div className="flex flex-wrap justify-between mt-8">
+            <div className="flex flex-wrap -mx-2 mt-8">
               {programs.map((program, index) => (
                 <AcadProgramCard program={program} key={index} />
               ))}
